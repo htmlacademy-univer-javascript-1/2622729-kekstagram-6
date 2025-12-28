9d946cc500e71062e61b49adc6fcfe5aa6e01af3
ben0982 <burman.ilya@yandex.ru>
2025-12-28 13:11:26 +0500

    module12-task1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 import { renderPictures } from './render-pictures.js';
 import { getData } from './api.js';
 import { showAlert } from './message.js';
+import { initFilters, getFilteredPictures } from './filters.js';
 import './form.js';
 import './image-edit.js';
 
 getData()
   .then((pictures) => {
-    renderPictures(pictures);
+    initFilters(pictures, renderPictures);
+    renderPictures(getFilteredPictures());
   })
   .catch((err) => {
     showAlert(err.message);
